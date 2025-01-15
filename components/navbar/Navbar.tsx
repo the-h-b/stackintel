25ecd3fa12639a8de06fd0dff1f4f40d3b9fd524
Calamity010 <89297033+Calamity010@users.noreply.github.com>
2025-01-15 12:46:22 +0530

    Update Navbar.tsx

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -42,7 +42,7 @@ const Navbar = () => {
               alt="hero-image"
               height={200}
               width={500}
-              className="h-auto w-[35%] md:w-[70%] lg:w-[65%] cursor-pointer"
+              className="h-auto w-[35%] md:w-[70%] lg:w-[55%] cursor-pointer"
             />
           </Link>
 
